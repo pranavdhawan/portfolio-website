36aca28de3963443c245f2e63126588424b70bfe
Pranav Dhawan <dhawan.pranav8@gmail.com>
2025-04-09 21:42:18 +0530

    projects updated

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,55 +5,62 @@ const PROJECTS: {
   repoUrl: string
   liveLink: string
 }[] = [
-  {
-    name: 'Matrix',
-    description: 'Inspired by the iconic Matrix movie terminal with dynamic visuals and quotes, built using TypeScript, CSS, NextTS, v0',
-    liveLink: 'https://matrix-one-pi.vercel.app/',
-    previewImage: '/matrix.png',
-    repoUrl: 'http://github.com/pranavdhawan/matrix',
-  },
-  {
-    name: 'Cli-mate',
-    description: 'Weather forecast for any city via the terminal. Build using NodeJS, CLI, Open-Meteo API',
-    liveLink: 'https://www.npmjs.com/package/@killkennyale/cli-mate',
-    previewImage: '/climate.png',
-    repoUrl: 'https://github.com/pranavdhawan/cli-mate',
-  },
-  {
-    name: 'Spotify Analyser ',
-    description: 'Analyse user’s Spotify and provide personalized AI generated musical insights and recommendations. Built using NodeJS, OpenAI, SpotifyAPI, and ReactJS',
-    liveLink: '',
-    previewImage: '/spotify.png',
-    repoUrl: 'https://github.com/pranavdhawan/spotify-aesthetic-analyser',
-  },
-  {
-    name: 'Road Mishap Detection System',
-    description: 'Comprehensive system for detecting and preventing road accidents with 3 essential modules: Speed Calculator, Gas Detection, and Crash Detection. Using Arduino, Web Development, IoT, and Embedded Systems',
-    liveLink: '',
-    previewImage: '/road.png',
-    repoUrl: 'https://github.com/pranavdhawan/Road-Mishap-Detection',
-  },
-  {
-    name: 'Hotel Booking Website',
-    description: 'A full stack website for Online Hotel Booking as a part of a MERN stack certification bootcamp course. Using React, MongoDB, Express, and NodeJS',
-    liveLink: 'https://book-your-hotel.onrender.com/',
-    previewImage: '/hotel.png',
-    repoUrl: 'https://github.com/pranavdhawan/hotel_frontend',
-  },
-  // {
-  //   name: 'Admin Ops Dashboard for Xyle',
-  //   description: 'Lorem ipsum dolor sit amet',
-  //   liveLink: 'https://google.com',
-  //   previewImage: '/xyle.png',
-  //   repoUrl: 'https://github.com',
-  // },
-  // {
-  //   name: 'User Dashboard for MonetiseUp',
-  //   description: 'Lorem ipsum dolor sit amet',
-  //   liveLink: 'https://www.dashboard.monetiseup.com/',
-  //   previewImage: '/monetise.png',
-  //   repoUrl: 'https://github.com',
-  // },
-]
+    {
+      name: 'Martin Scorcese Chatbot',
+      description: 'RAG chatbot that reflects, digresses, and occasionally rants like a man who’s spent his life chasing truth through cinema.',
+      liveLink: '',
+      previewImage: '/ss.png',
+      repoUrl: 'https://github.com/pranavdhawan/martin-scorcese',
+    },
+    {
+      name: 'Matrix',
+      description: 'Inspired by the iconic Matrix movie terminal with dynamic visuals and quotes, built using TypeScript, CSS, NextTS, v0',
+      liveLink: 'https://matrix-one-pi.vercel.app/',
+      previewImage: '/matrix.png',
+      repoUrl: 'http://github.com/pranavdhawan/matrix',
+    },
+    {
+      name: 'Cli-mate',
+      description: 'Weather forecast for any city via the terminal. Build using NodeJS, CLI, Open-Meteo API',
+      liveLink: 'https://www.npmjs.com/package/@killkennyale/cli-mate',
+      previewImage: '/climate.png',
+      repoUrl: 'https://github.com/pranavdhawan/cli-mate',
+    },
+    {
+      name: 'Spotify Analyser ',
+      description: 'Analyse user’s Spotify and provide personalized AI generated musical insights and recommendations. Built using NodeJS, OpenAI, SpotifyAPI, and ReactJS',
+      liveLink: '',
+      previewImage: '/spotify.png',
+      repoUrl: 'https://github.com/pranavdhawan/spotify-aesthetic-analyser',
+    },
+    {
+      name: 'Road Mishap Detection System',
+      description: 'Comprehensive system for detecting and preventing road accidents with 3 essential modules: Speed Calculator, Gas Detection, and Crash Detection. Using Arduino, Web Development, IoT, and Embedded Systems',
+      liveLink: '',
+      previewImage: '/road.png',
+      repoUrl: 'https://github.com/pranavdhawan/Road-Mishap-Detection',
+    },
+    {
+      name: 'Hotel Booking Website',
+      description: 'A full stack website for Online Hotel Booking as a part of a MERN stack certification bootcamp course. Using React, MongoDB, Express, and NodeJS',
+      liveLink: 'https://book-your-hotel.onrender.com/',
+      previewImage: '/hotel.png',
+      repoUrl: 'https://github.com/pranavdhawan/hotel_frontend',
+    },
+    // {
+    //   name: 'Admin Ops Dashboard for Xyle',
+    //   description: 'Lorem ipsum dolor sit amet',
+    //   liveLink: 'https://google.com',
+    //   previewImage: '/xyle.png',
+    //   repoUrl: 'https://github.com',
+    // },
+    // {
+    //   name: 'User Dashboard for MonetiseUp',
+    //   description: 'Lorem ipsum dolor sit amet',
+    //   liveLink: 'https://www.dashboard.monetiseup.com/',
+    //   previewImage: '/monetise.png',
+    //   repoUrl: 'https://github.com',
+    // },
+  ]
 
 export default PROJECTS
